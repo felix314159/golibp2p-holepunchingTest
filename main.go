6bb1c9fd6c46d6ce7033efa2417c8b023af53223
Jordan Belfort <felix314159@users.noreply.github.com>
2024-06-05 10:46:31 +0200

    Update main.go

--- main.go
+++ main.go
@@ -16,19 +16,21 @@ import (
 	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
 	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
 	libp2p "github.com/libp2p/go-libp2p"
-	libp2ptls "github.com/libp2p/go-libp2p/p2p/security/tls"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
-	//"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/p2p/security/noise"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+
 )
 
 const (
 	RendezvousString = "test18471398571"
 	contextTimeout = 60 * time.Second
 	sleepTime = 250 * time.Millisecond
-	version = "0.9.1" 
+	version = "0.9.1"
+
+	myTopic = "testtopic29329528"
 )
 
 var L *log.Logger
@@ -37,30 +39,12 @@ var L *log.Logger
 // USAGE:
 // 		export GOLOG_LOG_LEVEL=p2p-holepunch=debug && go run .
 
-// main searches for peers using rendezvous and kademlia and makes use of ipfs bootstrap servers to have nodes behind NAT connect to each other.
-// Then when the nodes find each other (can take up to 5 min), it should try to upgrade the relayed connections to direct connections using holepunching
-// PROBLEM: But the holepunching part does not work for me.. It seems to pretend that dcutr (direct connectio upgrade) protocol is not supported, but it is!
-// So i know that my network supports holepunching and that this program runs a node that will successfully ACCEPT holepunches, but it fails to INITIATE the holepunches?
+// EXPECTATION: run code on two nodes, after a while they find each other and receive each others topic messages
+// Scenario 1 (both nodes are in same LAN): it works as expected
+// Scenario 2 [sad ending] (the two nodes are not in the same LAN): after a while they find each other, but they never receive each others topic messages. A few (unrelated?) holepunching successes are printed and no holepunching failure. But when I use vole to ping vole shows success (took x ms), but the other node prints holepunching error: "error": "failed to open hole-punching stream: connection failed"
 
-// Testing setup: Node A runs this code in my private network, node B runs on another machine that uses usb-tethered mobile network from my phone to connect to node A.
-// Result: The nodes find each other via relayed connection, and holepunching via tools such as vole works, but if you run this go code on another machine outside your network the holepunching fails.. so how to do holepunching with go-libp2p correctly? I hoped it would happen automatically
-
-// Tested using currently newest go version: go1.22.3
-
-// To look up addresses of a peer by its ID (and ensure that it's publicly reachable) you can use at least 2 ways:
-//    1. install ipfs, start daemon using 'ipfs daemon', then run:   ipfs routing findpeer <nodeID>
-//    2. install rust and cargo install libp2p-lookup, then run: libp2p-lookup dht --network ipfs --peer-id <nodeID> [if it says thread 'main' panicked, then wait 2 minutes and try again]
-// IMPORTANT: you must append /p2p/<yourNodeID> when you want to try to connect to your node
-
-// To test whether holepunching works and see that your node is publicly available and supports the direct connection upgrade:
-//		1. build vole [https://github.com/ipfs-shipyard/vole] using: go install github.com/ipfs-shipyard/vole@latest
-//		2. run: 	cd "$(go env GOPATH)/bin" && vole libp2p ping <address> [here address is e.g. /ip4/139.178.65.157/tcp/4001/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa/p2p-circuit/p2p/<yourNodeID>]
-//			Note: It is important that the address ends with: p2p-circuit/p2p/<value>, so if the output of libp2p-lookup stops with p2p-circuit you must manually append the /p2p/<nodeIDprintedAtstartup>, if it does not work try it with another address.
-
-// BTW: "websocket: failed to close network connection" is a known libp2p issue that can be ignored
-// And why do you want direct connections? PubSub messages seem to not get distributed if you only have relayed connection.. (this is just a minimal example)
-
-func main() {
+// Is anyone able to find the flaw in scenario 2? Does it work with your setup? My setup is 1 node is using mobile data usb tethered (to not be in same network), the other node uses normal ethernet connection.
+func mainMinimalExample() {
 	// set up logger
 	L = log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
@@ -75,6 +59,7 @@ func main() {
 	}
 
 	// use ifps team hosted bootstrap servers (https://github.com/ipfs/kubo/blob/master/config/bootstrap_peers.go#L17)
+    ///*
     var bootstrapPeers []peer.AddrInfo
 	for _, multiAddr := range dht.DefaultBootstrapPeers {
 		// convert each multiaddr.MultiAddr to a peer.AddrInfo
@@ -84,27 +69,175 @@ func main() {
 		}
 		bootstrapPeers = append(bootstrapPeers, *addrInfo)
 	}
+	//*/
 
     // configure node
 	h, err := libp2p.New(
-		libp2p.Identity(priv),
-		libp2p.ListenAddrStrings(
-				"/ip4/0.0.0.0/tcp/0",
-				"/ip4/0.0.0.0/udp/9002/quic-v1",
-				"/ip6/::/udp/9002/quic-v1",
-		),
-		libp2p.Security(libp2ptls.ID, libp2ptls.New),	// support TLS
-		libp2p.Security(noise.ID, noise.New),			// support Noise
-		libp2p.DefaultTransports,						// support any default transports (includes quic)
+		libp2p.Identity(priv),							   // use generated private key as identity
 		libp2p.UserAgent(fmt.Sprintf("pouw/%v", version)), // set agent version so that other nodes know which version of this software this node is running
-		// e.g. libp2p-lookup dht --network ipfs --peer-id <nodeid> , then it shows the agent version among other listen addresses and supported protocols
+		
+		// options below make node publicly available if possible
+		// 		rust tool to verify e.g.: libp2p-lookup dht --network ipfs --peer-id <nodeid> , then it shows the agent version among other listen addresses and supported protocols
+		libp2p.EnableAutoRelayWithStaticRelays(bootstrapPeers), // become publicly available via ipfs hosted bootstrap nodes that act as relay servers
+		libp2p.EnableHolePunching(), 	// enable holepunching
+        libp2p.NATPortMap(), 			// try to use upnp to open port in firewall
 
-        // punch through firewall with help of ipfs hosted relay servers
-        libp2p.NATPortMap(),
-        libp2p.EnableHolePunching(),
-        libp2p.EnableAutoRelayWithStaticRelays(bootstrapPeers),
-        libp2p.EnableRelay(), // enabled by default but who cares
-        libp2p.EnableNATService(),
+        // ---- this will be used implicitely ----
+        /*
+		libp2p.ListenAddrStrings( // as specified in defaults.go
+			"/ip4/0.0.0.0/tcp/0",
+			"/ip4/0.0.0.0/udp/0/quic-v1",
+			"/ip4/0.0.0.0/udp/0/quic-v1/webtransport",
+			"/ip6/::/tcp/0",
+			"/ip6/::/udp/0/quic-v1",
+			"/ip6/::/udp/0/quic-v1/webtransport",
+		),
+		*/
+
+        //libp2p.DefaultSecurity,							// support TLS and Noise
+		//libp2p.DefaultTransports,						// support any default transports (tcp, quic, websocket and webtransport)
+		//libp2p.DefaultMuxers, 							// support yamux
+		//libp2p.DefaultPeerstore, 						// use default peerstore (probably not necessary)
+
+		// ---- tried these but not necessary afaik ----
+
+        //libp2p.EnableRelayService(), 	// if you are publicly available act as a relay and help others behind NAT
+        //libp2p.EnableNATService(), 	// if you are publicly available help other to figure out whether they are publicly available or not
+
+        //libp2p.EnableRelay(), 			// makes no difference (because it is enabled by default)
+        //libp2p.ForceReachabilityPublic(), // do NOT use this when you are behind NAT, i can't even ping my node with this enabled
+	)
+	if err != nil {
+		L.Panic(err)
+	}
+
+	// print information about node
+	myNodeID := h.ID().String()
+	L.Printf("My node ID: %v", myNodeID)
+
+	/*
+	// print node addresses after 3 seconds
+	go func() {
+		time.Sleep(3 * time.Second)
+
+		L.Printf("I am reachable via these addresses:")
+		for _, addr := range h.Addrs() {
+			L.Printf("\t%v/p2p/%v", addr, h.ID().String())
+		}
+	}()
+	*/
+
+	// with lots of trial and error i found out that there is one ipfs hosted bootstrap server that usually works for holepunching (many other don't):
+	reliableIPFSserver := "/ip4/139.178.65.157/tcp/4001/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa/p2p-circuit"
+	fullAddressToThisNode := fmt.Sprintf("%v/p2p/%v", reliableIPFSserver, myNodeID)
+	L.Printf("Run the following command on a machine in a different network to verify that holepunching works (if it fails wait a min and try again):\n\tvole libp2p ping %v\n\n", fullAddressToThisNode)
+	// on success it will print 3x: 'Took <time>ms', this will prove that holepunching works with your network
+
+	// let user cancel execution by sending SIGINT or SIGTERM (prevents issue where ctrl+C or ctrl+. don't kill running program)
+	ctx, cancel := context.WithCancel(context.Background())
+    sigs := make(chan os.Signal, 1)
+    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+    go func() {
+    	<-sigs
+        cancel()
+    }()
+	
+
+	// use gossipsub for message distribution over topics, returns *pubsub.PubSub (using default GossipSubRouter as the router)
+	ps, err := pubsub.NewGossipSub(ctx, h)
+	if err != nil {
+		L.Panic(err)
+	}
+
+	// get topic handler
+	myTopicHandler, err := ps.Join(myTopic)
+	if err != nil {
+		L.Panic(err)
+	}
+
+	// subscribe to topic
+	sub, err := myTopicHandler.Subscribe()
+	if err != nil {
+		L.Panic(err)
+	}
+	L.Printf("Successfully subscribed to topic: %v\n", myTopic)
+
+	// listen to topic messages
+	go TopicReceiveMessage(ctx, sub, h)	
+
+	// send message below to topic in regular intervals
+	spamThisTopicMessage := fmt.Sprintf("hi topic enjoyers, my peer id is %v", myNodeID)
+	go TopicSendMessage(ctx, spamThisTopicMessage, myTopicHandler)
+
+	// try to find peers
+	DiscoverPeers(ctx, h)
+	<-ctx.Done() // run until manually cancelled
+
+	// run code with two nodes and just wait a few min, as soon as the the nodes find each other you start seeing their topic messages
+}
+
+func mainPubsubExample() {
+	// set up logger
+	L = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// generate key pair
+	var err error
+	priv, _, err := crypto.GenerateKeyPair(
+		crypto.Ed25519,
+		-1,
+	)
+	if err != nil {
+		L.Panic(err)
+	}
+
+	// use ifps team hosted bootstrap servers (https://github.com/ipfs/kubo/blob/master/config/bootstrap_peers.go#L17)
+    ///*
+    var bootstrapPeers []peer.AddrInfo
+	for _, multiAddr := range dht.DefaultBootstrapPeers {
+		// convert each multiaddr.MultiAddr to a peer.AddrInfo
+		addrInfo, err := peer.AddrInfoFromP2pAddr(multiAddr)
+		if err != nil {
+			L.Panicf("failed to convert MultiAddr to AddrInfo: %v", err)
+		}
+		bootstrapPeers = append(bootstrapPeers, *addrInfo)
+	}
+	//*/
+
+    // configure node
+	h, err := libp2p.New(
+		libp2p.Identity(priv),							   // use generated private key as identity
+		libp2p.UserAgent(fmt.Sprintf("pouw/%v", version)), // set agent version so that other nodes know which version of this software this node is running
+		
+		// options below make node publicly available if possible
+		// 		rust tool to verify e.g.: libp2p-lookup dht --network ipfs --peer-id <nodeid> , then it shows the agent version among other listen addresses and supported protocols
+		libp2p.EnableAutoRelayWithStaticRelays(bootstrapPeers), // become publicly available via ipfs hosted bootstrap nodes that act as relay servers
+		libp2p.EnableHolePunching(), 	// enable holepunching
+        libp2p.NATPortMap(), 			// try to use upnp to open port in firewall
+
+        // ---- this will be used implicitely ----
+        /*
+		libp2p.ListenAddrStrings( // as specified in defaults.go
+			"/ip4/0.0.0.0/tcp/0",
+			"/ip4/0.0.0.0/udp/0/quic-v1",
+			"/ip4/0.0.0.0/udp/0/quic-v1/webtransport",
+			"/ip6/::/tcp/0",
+			"/ip6/::/udp/0/quic-v1",
+			"/ip6/::/udp/0/quic-v1/webtransport",
+		),
+		*/
+
+        //libp2p.DefaultSecurity,							// support TLS and Noise
+		//libp2p.DefaultTransports,						// support any default transports (tcp, quic, websocket and webtransport)
+		//libp2p.DefaultMuxers, 							// support yamux
+		//libp2p.DefaultPeerstore, 						// use default peerstore (probably not necessary)
+
+		// ---- tried these but not necessary afaik ----
+
+        //libp2p.EnableRelayService(), 	// if you are publicly available act as a relay and help others behind NAT
+        //libp2p.EnableNATService(), 	// if you are publicly available help other to figure out whether they are publicly available or not
+
+        //libp2p.EnableRelay(), 			// makes no difference (because it is enabled by default)
+        //libp2p.ForceReachabilityPublic(), // do NOT use this when you are behind NAT, i can't even ping my node with this enabled
 	)
 	if err != nil {
 		L.Panic(err)
@@ -143,7 +276,9 @@ func main() {
 	
 	// try to find peers
 	DiscoverPeers(ctx, h)
-	<-ctx.Done()
+	<-ctx.Done() // run until manually cancelled
+
+	// run code with two nodes and just wait a few min, they should find each other
 }
 
 // DiscoverPeers uses Rendezvous and Kademlia to discover peers, tries to upgrade relayed connections to direct connections using holepunching (but this part does not work for me)
@@ -190,19 +325,6 @@ func DiscoverPeers(ctx context.Context, h host.Host) {
 					continue
 				} 
 
-				// ---- I NEED HELP WITH THE FOLLOWING PART (it keeps saying: "error": "failed to open hole-punching stream: failed to negotiate protocol: protocols not supported: [/libp2p/dcutr]") but when i use libp2p-lookup dht --network ipfs --peer-id <nodeID> it tells me that that protocol is supported [and everyone else is able to holepunch too, so this code must be wrong] ----
-
-				/* maybe you should not manually try to do that because it is supposed to happen automatically?
-				// try to establish direct connection (not relayed connection)
-				err = holePunchConnect(ctx, h, peer, true)
-				if err != nil {
-					L.Printf("Failed to upgrade to direct connection: %v", err)
-				} else { // Goal: Have it print the below :)
-					L.Printf("Successfully holepunched to upgrade to direct connection with %v", peer.ID.String())
-					break
-				}
-				*/
-
 				// ok you successfully connected to new peer, remember this peer (libp2p's who-is-connected-list seems to contain many inactive nodes)
 				myConnectedPeers[peer.ID.String()] = true
 				L.Printf("Connected to: %v", peer.ID.String())
@@ -235,26 +357,10 @@ func DiscoverPeers(ctx context.Context, h host.Host) {
 	}
 }
 
-/*
-// holePunchConnect tries to upgrade relayed connection to direct connection
-// Mainly taken from here: https://github.com/libp2p/go-libp2p/blob/master/p2p/protocol/holepunch/util.go#L67
-func holePunchConnect(ctx context.Context, host host.Host, pi peer.AddrInfo, isClient bool) error {
-	holePunchCtx := network.WithSimultaneousConnect(ctx, isClient, "hole-punching")
-	forceDirectConnCtx := network.WithForceDirectDial(holePunchCtx, "hole-punching")
-	dialCtx, cancel := context.WithTimeout(forceDirectConnCtx, contextTimeout)
-	defer cancel()
-
-	if err := host.Connect(dialCtx, pi); err != nil {
-		return fmt.Errorf("hole punch attempt with peer %v was unsuccessful: %v", pi.ID, err)
-	}
-
-	return nil
-}
-*/
-
 // InitDHT connects to a few known default bootstrap nodes (they enable distributed node discovery and are the only centralized necessity in kademlia DHT).
 func InitDHT(ctx context.Context, h host.Host) *dht.IpfsDHT {
-	kademliaDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeAuto))	
+	kademliaDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeAuto))
+	//kademliaDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeAutoServer))	// My Testing: ModeServer -> NATed Node can't be reached at all, ModeAutoServer -> 
 	if err != nil {
 		L.Panic(err)
 	}
@@ -279,4 +385,49 @@ func InitDHT(ctx context.Context, h host.Host) *dht.IpfsDHT {
 	wg.Wait()
 
 	return kademliaDHT
+}
+
+// ---- Topic stuff ----
+
+// TopicReceiveMessage is responsible for send messages to the topic (sends every 5 seconds).
+func TopicSendMessage(ctx context.Context, msgContent string, targetTopic *pubsub.Topic) {
+	ticker := time.NewTicker(5 * time.Second)
+	for range ticker.C {
+
+		err := targetTopic.Publish(ctx, []byte(msgContent))
+		if err != nil {
+			L.Panicf("Failed to send msg to topic due to: %v\n", err)
+		} else {
+			L.Printf("Successfully sent message to topic")
+		}
+
+	}
+}
+
+// TopicReceiveMessage is responsible for receiving topic messages (checks every 5 ms).
+func TopicReceiveMessage(ctx context.Context, sub *pubsub.Subscription, h host.Host) {
+	ticker := time.NewTicker(5 * time.Millisecond)
+
+	for range ticker.C {
+		m, err := sub.Next(ctx)
+		if err != nil {
+			L.Panic(err)
+		}
+
+		// in a simple 2 node demonstration this helps filter out messages you have received that you have sent yourself. but in more complex example you need to find better approach as ReceivedFrom only means who forwarded the message to you, it might or might not be the original sender and with many nodes you might get your own message back (gossiping aspect of gossipsub)
+		if m.ReceivedFrom.String() == h.ID().String() {
+			L.Printf("Received topic message that was sent by myself")
+			continue
+		}
+
+		t := sub.Topic()
+		L.Printf("Topic: %v - Received message: %v\n", t, string(m.Data))
+
+	}
+}
+
+// choose which example to run
+func main() {
+	//mainMinimalExample()
+	mainPubsubExample()
 }
